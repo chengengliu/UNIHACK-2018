74ed01e01517b2dbebb860cd22a4715956818420
samuel-x <roflollies@users.noreply.github.com>
2018-07-29 08:49:16 +1000

    Retreived the old MainActivity.java

--- app/src/main/java/com/example/zacharyho/myunihack/MainActivity.java
+++ app/src/main/java/com/example/zacharyho/myunihack/MainActivity.java
@@ -1,99 +1,147 @@
 package com.example.zacharyho.myunihack;
-
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.PointF;
+import android.support.annotation.NonNull;
 import android.util.Log;
-import android.view.MenuItem;
-import android.widget.TextView;
 
-import com.example.zacharyho.myunihack.routing.AStar;
-import com.example.zacharyho.myunihack.routing.DataParser;
-import com.example.zacharyho.myunihack.routing.helperobjects.Coordinate;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-
-import static android.content.ContentValues.TAG;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-    private TextView testMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-
-
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-
-
-
-            return false;
-        }
-    };
-
+import org.json.*;
+import com.google.gson.JsonElement;
+import com.mapbox.geojson.Feature;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.net.ssl.HttpsURLConnection;
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
+        MapboxMap.OnMapClickListener {
+    private MapView mapView;
+    private Marker featureMarker;
+    private MapboxMap mapboxMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1IjoieWhvNCIsImEiOiJjams1NXRxdDIwZGZ1M3dwMmIzbnB3ODN4In0.6yez7PFT9gg4d7AVIl1V8w");
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-//        DataParser.readData();
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+        try {
+            drawAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        ValueEventListener postListener = new ValueEventListener() {
-            //            Log.d("Listening", "Listening!");
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                DataParser.readData(dataSnapshot);
-//                String[] testPath = AStar.getBestPath(
-////                        new Coordinate(-37.800449, 144.963938),
-////                        new Coordinate(-37.802693, 144.973985)
-//                        new Coordinate(-37.800, 144.963),
-//                        new Coordinate(-37.802, 144.973)
-//                );
-//
-//                for (String c : testPath) {
-//                    System.out.printf("Next: %s)\n", c);
-//                }
-
-                Coordinate[] testPath = AStar.getBestPath(
-                        new Coordinate(-37.800, 144.963),
-                        new Coordinate(-37.802, 144.973)
+    public void onMapReady(MapboxMap mapboxMap) {
+        MainActivity.this.mapboxMap = mapboxMap;
+        mapboxMap.addOnMapClickListener(this);
+    }
+    @Override
+    public void onMapClick(@NonNull LatLng point) {
+        if (featureMarker != null) {
+            mapboxMap.removeMarker(featureMarker);
+        }
+        final PointF pixel = mapboxMap.getProjection().toScreenLocation(point);
+        List<Feature> features = mapboxMap.queryRenderedFeatures(pixel);
+        if (features.size() > 0) {
+            Feature feature = features.get(0);
+            String property;
+            StringBuilder stringBuilder = new StringBuilder();
+            if (feature.properties() != null) {
+                for (Map.Entry<String, JsonElement> entry : feature.properties().entrySet()) {
+                    stringBuilder.append(String.format("%s - %s", entry.getKey(), entry.getValue()));
+                    stringBuilder.append(System.getProperty("line.separator"));
+                }
+                featureMarker = mapboxMap.addMarker(new MarkerOptions()
+                        .position(point)
+                        .title(getString(R.string.query_feature_marker_title))
+                        .snippet(stringBuilder.toString())
                 );
-                Log.d("asd", "asd");
+            } else {
+                property = getString(R.string.query_feature_marker_snippet);
+                featureMarker = mapboxMap.addMarker(new MarkerOptions()
+                        .position(point)
+                        .snippet(property)
+                );
             }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // Getting Post failed, log a message
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
-                // ...
-            }
-        };
-
-        FirebaseAdapter.myRef.addListenerForSingleValueEvent(postListener);
+        } else {
+            featureMarker = mapboxMap.addMarker(new MarkerOptions()
+                    .position(point)
+                    .snippet(getString(R.string.query_feature_marker_snippet))
+            );
+        }
+        mapboxMap.selectMarker(featureMarker);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+    public String drawAll()  throws Exception {
+        String routeStr = "";
+        String httpsURL = createRequest("walking", "-37.802078,144.966681", "-37.800748,144.966905");
+        URL myUrl = new URL(httpsURL);
+        HttpsURLConnection conn = (HttpsURLConnection)myUrl.openConnection();
+        InputStream is = conn.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String inputLine;
+        while ((inputLine = br.readLine()) != null) {
+            routeStr = inputLine;
+        }
+        br.close();
+        JSONObject obj = new JSONObject(routeStr);
+        String coordinates = obj.getJSONObject("route").getJSONObject("geometry").getString("coordinates");
+        Log.d("Print", coordinates);
+        return coordinates;
+    }
+    private String createRequest(String profile, String coodinate1, String coodinate2) {
+        String key = "access_token=pk.eyJ1IjoicmRpYW9zdHVkZW50IiwiYSI6ImNqazU5Y3dxMDBtenMzcHBqdjlncWt5YTYifQ.K8YgUZPXPJ6xwuJrFoyM3g";
+        String httpStr = "https://api.mapbox.com/directions/v5/mapbox/";
+        httpStr = httpStr + coodinate1 + ";" + coodinate2 + "?geometries=geojson&" + key;
+        return httpStr;
     }
 }
